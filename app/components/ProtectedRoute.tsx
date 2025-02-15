a3112cb9a5f72ead7cce17cf4796dbf3152b42bb
ToobaSaleem1234 <168776853+ToobaSaleem1234@users.noreply.github.com>
2025-02-15 21:27:24 +0500

    Add files via upload

---
+++ app/components/ProtectedRoute.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+
+export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
+    const router = useRouter()
+    useEffect(() => {
+        const isLoggedIn = localStorage.getItem('isLoggedIn')
+        if (!isLoggedIn) {
+            router.push('/admin/dashboard')
+        }
+    }, [router])
+    return <>{children}</>
+};
